7fe51f4ee7cf39bfdb6b069f3f268f38da3e83c0
Rishabh-Truwix <rishabhr@truwix.com>
2025-03-19 14:41:49 +0530

    add zipcode model and routes

---
+++ src/common/helpers/zipcode.helper.js
@@ -0,0 +1,9 @@
+
+'use strict';
+
+import Zipcode from '../models/zipcode';
+import BaseHelper from  "./baseHelper.js";
+
+const zipcodeHelper = new BaseHelper(Zipcode);
+
+export default zipcodeHelper;
